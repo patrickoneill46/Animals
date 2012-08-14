7bec23152c0092c52fc404381fd99f6a50dace16
patrick.oneill@bearingpoint.com <patrick.oneill@bearingpoint.com>
2012-08-14 13:48:53 +0000

    update file /client/default/js/controller/Main.js

--- client/default/js/controller/Main.js
+++ client/default/js/controller/Main.js
@@ -91,7 +91,7 @@ Ext.define('Animals.controller.Main', {
       }
     }, function (res){
 
-      console.log(res);
+      //console.log(res);
       var photos = JSON.parse(res.body).photos;
       console.log(photos);
       var data = [];
